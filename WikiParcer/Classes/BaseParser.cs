d1869b036c5706a1de1befe79050b4a6425d5335
Александр Малых <xen-rus@yandex.ru>
2021-08-26 23:39:05 +0300

    Get internal links to prase

--- WikiParcer/Classes/BaseParser.cs
+++ WikiParcer/Classes/BaseParser.cs
@@ -14,14 +14,18 @@ namespace WikiParcer.Classes
     {
         string Link { get; set; }
 
+        private int wordsCount;
+
+        List<string> links = new List<string>();
+
         public List<string> GetChaildLinks()
         {
-            throw new NotImplementedException();
+            return links;
         }
 
         public int GetWordCount()
         {
-            throw new NotImplementedException();
+            return wordsCount;
         }
 
         public async void ParceAsync()
@@ -31,13 +35,26 @@ namespace WikiParcer.Classes
             web.OverrideEncoding = Encoding.UTF8;
             var doc = web.Load(url);
 
-           //
-            var getBody = doc.DocumentNode.SelectSingleNode("//body");
-            var htmlwoutBdi = Regex.Replace(getBody.InnerHtml, @"<bdi.*?>(.*?)<\/bdi>", string.Empty).Trim();
-            var htmlwoutScript = Regex.Replace(htmlwoutBdi, @"<script.*?>(.*?)<\/script>", string.Empty).Trim(); ;
+            links.Clear();
+
+
+                // Get the value of the HREF attribute
+            foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
+                {
+                    string hrefValue = link.GetAttributeValue("href", string.Empty);
+                    links.Add(hrefValue);
+                }
+                //
+                var getBody = doc.DocumentNode.SelectSingleNode("//body");
+
+            var bigRegexPattern = Regex.Replace(getBody.InnerHtml, @"(<bdi.*?>((.|\n)*?)<\/bdi>)|(<script.*?>((.|\n)*?)<\/script>)|(<style.*?>((.|\n)*?)<\/style>)", string.Empty).Trim();
+
+       /*     var htmlwoutBdi = Regex.Replace(getBody.InnerHtml, @"<bdi.*?>((.|\n)*?)<\/bdi>", string.Empty).Trim();
+            var htmlwoutScript = Regex.Replace(htmlwoutBdi, @"<script.*?>((.|\n)*?)<\/script>", string.Empty).Trim(); 
+            var htmlwoutStyle = Regex.Replace(htmlwoutScript, @"<style.*?>((.|\n)*?)<\/style>", string.Empty).Trim(); ;*/
 
             var doc1 = new HtmlDocument();
-            doc1.LoadHtml(htmlwoutScript);
+            doc1.LoadHtml(bigRegexPattern);
             var cleanBody =  doc1.DocumentNode.InnerText;
 
             cleanBody = cleanBody.Replace('\n', ' ');
@@ -45,6 +62,7 @@ namespace WikiParcer.Classes
 
             var strings = cleanBody.Split().Where(line => line != "" && (line.IndexOf("&") == -1)).AsParallel();
 
+            wordsCount = strings.Count();
         }
 
         public void Route(string link)
