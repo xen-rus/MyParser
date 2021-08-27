8d3e87fd1427c6710b6dc377885e6d5c1c66a920
xen-rus <xen-rus@yandex.ru>
2021-08-27 23:07:34 +0300

    Update Parser

--- WikiParcer/Classes/BaseParser.cs
+++ WikiParcer/Classes/BaseParser.cs
@@ -18,10 +18,37 @@ namespace WikiParcer.Classes
 
         List<string> links = new List<string>();
 
-        public List<string> GetChaildLinks()
+        private HtmlDocument htmlDoc;
+
+        public  List<string> GetChaildLinks()
         {
+            links?.Clear();
+            // Get the value of the HREF attribute
+            foreach (HtmlNode link in htmlDoc.DocumentNode.SelectNodes("//a[@href]"))
+            {
+                string hrefValue =link.GetAttributeValue("href", string.Empty);
+
+                if(hrefValue.Contains("http:") || 
+                   hrefValue.Contains("https:"))
+                    links.Add(hrefValue);
+                else
+                    links.Add("https:" + hrefValue);
+
+            }
+
             return links;
         }
+
+        public BaseParser(string mainLink)
+        {
+            Link = mainLink;
+            var web = new HtmlWeb();
+            web.OverrideEncoding = Encoding.UTF8;
+            htmlDoc  = web.Load(mainLink);
+        }
+
+
+
 
         public int GetWordCount()
         {
@@ -30,31 +57,16 @@ namespace WikiParcer.Classes
 
         public async void ParceAsync()
         {
-            var url = Link;
-            var web = new HtmlWeb();
-            web.OverrideEncoding = Encoding.UTF8;
-            var doc = web.Load(url);
+            var getBody = htmlDoc.DocumentNode.SelectSingleNode("//body");
 
-            links.Clear();
-
-
-                // Get the value of the HREF attribute
-            foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
-                {
-                    string hrefValue = link.GetAttributeValue("href", string.Empty);
-                    links.Add(hrefValue);
-                }
-                //
-                var getBody = doc.DocumentNode.SelectSingleNode("//body");
-
-            var bigRegexPattern = Regex.Replace(getBody.InnerHtml, @"(<bdi.*?>((.|\n)*?)<\/bdi>)|(<script.*?>((.|\n)*?)<\/script>)|(<style.*?>((.|\n)*?)<\/style>)", string.Empty).Trim();
+            var splitBody = Regex.Replace(getBody.InnerHtml, @"(<bdi.*?>((.|\n)*?)<\/bdi>)|(<script.*?>((.|\n)*?)<\/script>)|(<style.*?>((.|\n)*?)<\/style>)|(([0-9]|\n)*?)", string.Empty, RegexOptions.IgnoreCase).Trim();
 
        /*     var htmlwoutBdi = Regex.Replace(getBody.InnerHtml, @"<bdi.*?>((.|\n)*?)<\/bdi>", string.Empty).Trim();
             var htmlwoutScript = Regex.Replace(htmlwoutBdi, @"<script.*?>((.|\n)*?)<\/script>", string.Empty).Trim(); 
             var htmlwoutStyle = Regex.Replace(htmlwoutScript, @"<style.*?>((.|\n)*?)<\/style>", string.Empty).Trim(); ;*/
 
             var doc1 = new HtmlDocument();
-            doc1.LoadHtml(bigRegexPattern);
+            doc1.LoadHtml(splitBody);
             var cleanBody =  doc1.DocumentNode.InnerText;
 
             cleanBody = cleanBody.Replace('\n', ' ');
--- WikiParcer/Program.cs
+++ WikiParcer/Program.cs
@@ -2,17 +2,35 @@
 using WikiParcer.Classes;
 using WikiParcer.Interfaces;
 
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace WikiParcer
 {
     class Program
     {
+        static object _lock = new object();
         static void Main(string[] args)
         {
-            IParser WikiParcer = new BaseParser();
+            const string WikiLink = "http://www.wikipedia.org";
 
-            WikiParcer.Route("http://www.wikipedia.org");
-
+            IParser WikiParcer = new BaseParser(WikiLink);
+                        
             WikiParcer.ParceAsync();
+
+            Console.WriteLine($"Link =" + WikiLink + "wodrs count " + WikiParcer.GetWordCount());
+
+            Parallel.ForEach(WikiParcer.GetChaildLinks(), (link) => {
+
+                IParser ChildWiiParcer = new BaseParser(link);
+
+                ChildWiiParcer.ParceAsync();
+
+                Console.WriteLine($"Link =" + link + "wodrs count " + ChildWiiParcer.GetWordCount());
+                
+
+            });
+        
         }
     }
 }
